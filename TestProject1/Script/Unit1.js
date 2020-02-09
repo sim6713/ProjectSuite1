881cb38ea0418b5f2a13d1a21f4de9ada9440ef6
ALIAKSEI SIMUKOU <sim6713@rambler.ru>
2020-02-10 01:33:08 +0300

    start application. maximize application

--- TestProject1/Script/Unit1.js
+++ TestProject1/Script/Unit1.js
@@ -1,14 +1,50 @@
 ﻿
+
 // The main test function
 function Main()
 {
   let process = RunApplication();
+  MaximizeApp(process);
   
-  //Close(process);
+  CloseApp(process);
 }
 
 // Run the tested application
 function RunApplication()
 {
   return TestedApps.TestTD32.Run();
+}
+
+
+function MaximizeApp(process)
+{
+  w = process.WaitWindow("TFMain", "FMain", -1, 5000);
+  
+  if (w.Exists)
+    {
+      w.Activate();
+      Log.Picture(w, "MaximizeApp");
+    }
+    else
+      Log.Warning("Incorrect window - MaximizeApp");
+      
+  w.Maximize();
+}
+
+function CloseApp(process)
+{
+    let p = process;
+    // Waits for the window for 5 seconds
+    w = p.WaitWindow("TFMain", "FMain", -1, 5000);
+    
+    if (w.Exists)
+    {
+      w.Activate();
+      Log.Picture(w, "CloseApp");
+    }
+    else
+      Log.Warning("Incorrect window - CloseApp");
+  
+  // Close the application
+  w.Close(0);
 }
