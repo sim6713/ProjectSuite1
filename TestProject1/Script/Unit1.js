a557569beaa1a886bef9e3f92baf44dde7c33ea3
ALIAKSEI SIMUKOU <sim6713@rambler.ru>
2020-02-10 00:42:45 +0300

    first commit

---
+++ TestProject1/Script/Unit1.js
@@ -0,0 +1,14 @@
+﻿
+// The main test function
+function Main()
+{
+  let process = RunApplication();
+  
+  //Close(process);
+}
+
+// Run the tested application
+function RunApplication()
+{
+  return TestedApps.TestTD32.Run();
+}
