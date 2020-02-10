d0524a08ea7172b2e237822f818a04659818db97
ALIAKSEI SIMUKOU <sim6713@rambler.ru>
2020-02-10 21:15:17 +0300

    'CheckComboBox'

--- TestProject1/Script/Unit1.js
+++ TestProject1/Script/Unit1.js
@@ -1,4 +1,13 @@
 ﻿
+let FMain = Aliases.TestTD32.FMain;
+
+let MainPanel = FMain.MDIClient.FEdits.sbMain.pMainPanel;
+let ButtonsPanel = FMain.MDIClient.FEdits.sbMain.pButtonsPanel;
+let LabelsPanel = FMain.MDIClient.FEdits.sbMain.pLabelsPanel;
+
+let TextLong = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...";
+let TextShort = "Lorem ipsum dolor...";
+
 // The main test function
 function Main()
 {
@@ -6,7 +15,13 @@ function Main()
   let WinMain = MaximizeApp(process);
   OpenFormEdit(WinMain);
   
-  Delay(3000);
+  FuncMemoEdit();
+  FuncTextEdit();
+  FuncCheckBox();
+  FuncCurrencyEdit();
+  FuncCheckComboBox();
+  FuncSpinEdit();
+
   CloseApp(process);
 }
 
@@ -44,6 +59,107 @@ function OpenFormEdit(w)
     }
     else
       Log.Warning("Incorrect window - OpenFormEdit");   
+}
+///////////////////////////////////////////////////////////////////////////////
+//Перейти в первое поле 'Memo'
+//Ввести произвольный многострочный текст в поле 'Memo'
+//Нажать на кнопку "Значение"
+//Проверить появление значения справа от кнопки
+function FuncMemoEdit()
+{
+  ObjectInputText(MainPanel.mTest.mTestEdit.TcxCustomInnerMemo, "MemoEdit", TextLong);
+  ObjectClickButton(ButtonsPanel.TcxButton, "MemoEdit");
+  aqObject.CheckProperty(LabelsPanel.TcxLabel, "WndCaption", cmpEqual, TextLong, true);
+}
+/////////////////////////////////////////////////////////////////
+//Ввести произвольное значение в поле 'TextEdit'
+//Нажать на кнопку "Значение"
+//Проверить появление значения справа от кнопки
+function FuncTextEdit()
+{
+  ObjectInputText(MainPanel.edTest.edTestEdit.TcxCustomInnerTextEdit, "TextEdit", TextShort);
+  ObjectClickButton(ButtonsPanel.TcxButton2, "TextEdit");
+  aqObject.CheckProperty(LabelsPanel.TcxLabel2, "WndCaption", cmpEqual, TextShort, true);
+}
+/////////////////////////////////////////////////////////////////
+//Нажать на кнопку "Значение" справа от 'CheckBox'
+//Проверить появление значения справа от кнопки
+//Поставить галку в поле 'CheckBox'
+//Нажать на кнопку "Значение"
+//Проверить появление значения справа от кнопки
+function FuncCheckBox()
+{
+  ObjectClickButton(ButtonsPanel.TcxButton3, "CheckBoxClickButton");
+  aqObject.CheckProperty(LabelsPanel.TcxLabel3, "WndCaption", cmpEqual, "False", false);
+  ObjectClickButton(MainPanel.cbTest.cbTestEdit, "CheckBoxChecked");
+  ObjectClickButton(ButtonsPanel.TcxButton3, "CheckBoxClickButton");
+  aqObject.CheckProperty(LabelsPanel.TcxLabel3, "WndCaption", cmpEqual, "True", false);
+}
+/////////////////////////////////////////////////////////////////
+//Ввести произвольное значение в поле 'CurrencyEdit'
+//Нажать на кнопку "Значение"
+//Проверить появление значения справа от кнопки
+function FuncCurrencyEdit()
+{
+  let input_value = "123";
+  ObjectInputText(MainPanel.ceTest.ceTestEdit.TcxCustomInnerTextEdit, "CurrencyEditInputValue", input_value);
+  ObjectClickButton(ButtonsPanel.TcxButton4, "CurrencyEditClickButton");
+  aqObject.CheckProperty(LabelsPanel.TcxLabel4, "WndCaption", cmpEqual, input_value, false);
+}
+/////////////////////////////////////////////////////////////////
+//Развернуть список и выбрать произвольное значение в поле 'CheckComboBox'
+//Нажать на кнопку "Значение"
+//Проверить появление значения справа от кнопки
+function FuncCheckComboBox()
+{ 
+  let tcxCheckComboBox = MainPanel.ccbTest.ccbTestEdit;
+  ObjectClickButton(tcxCheckComboBox, "CheckComboBoxClick");
+  
+  tcxCheckComboBox.TcxCustomComboBoxInnerEdit.Keys("[Down] [Enter]");    
+  ObjectClickButton(ButtonsPanel.TcxButton5, "CheckComboBoxClickButton");
+  
+  aqObject.CheckProperty(LabelsPanel.TcxLabel5, "WndCaption", cmpContains, "1", false);
+  Log.Picture(LabelsPanel.TcxLabel5, "CheckComboBoxCheckValue");  
+}
+/////////////////////////////////////////////////////////////////
+//Ввести произвольное значение в поле 'SpinEdit'
+//Увеличить или уменьшить значение в поле 'SpinEdit' с помощью стрелок
+//Нажать на кнопку "Значение"
+//Проверить появление значения справа от кнопки
+function FuncSpinEdit()
+{
+  
+}
+
+//Ввести тeкст
+function ObjectInputText(wnd, message, text)
+{
+  if (wnd.Exists)
+  {
+    wnd.Click();
+    wnd.Keys(text);
+           
+    Log.Picture(wnd, message);
+  }
+  else
+  {
+    Log.Warning("Incorrect window - " + message);
+  }      
+}
+
+//Нажать на кнопку
+function ObjectClickButton(wndButton, message)
+{
+  if(wndButton)
+  {
+    wndButton.Click();    
+    Delay(1000);           
+    Log.Picture(wndButton, message);
+  }
+  else
+  {
+    Log.Warning("Incorrect window - " + message);
+  }      
 }
 
 
